f711d913fa106e61833eb068470b3160fec08c08
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-09 07:42:41 +0600

    O(N) complexity example

---
+++ Module-1/O_N.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i += 2)
+    {
+        sum += a[i];
+        // if (i % 2 > 0)
+        // {
+        //     sum += a[i];
+        // }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
