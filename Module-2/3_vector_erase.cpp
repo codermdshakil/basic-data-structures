524163b2ce6e61ab259d5d0aad1007c1665757da
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-10 21:06:06 +0600

    learn: erase or delete a single element, delete a posion, delete a specific index to end

---
+++ Module-2/3_vector_erase.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+    // delete a single element
+    // v.erase(v.begin() + 2); // delete 3
+
+    // delete a porsion index 3 to 5 
+    // v.erase(v.begin()+3, v.begin()+5); // delete 4,5
+
+    // delete a specific index to end
+    v.erase(v.begin()+3, v.end()); // delete including index 3 to end
+
+    for (int c : v)
+    {
+        cout << c << " ";
+    }
+    return 0;
+}
