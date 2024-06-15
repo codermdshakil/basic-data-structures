34e728eca4a7447beaa4d10f8c0a623fb027047a
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-15 14:09:57 +0600

    Solved: New Array problem.

---
+++ Module-2.5/L_New_Array.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> b(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    vector<int> c;
+    for (int i = 0; i < n; i++)
+    {
+        c.push_back(b[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        c.push_back(a[i]);
+    }
+    
+
+    for (int i = 0; i < c.size(); i++)
+    {
+        cout << c[i] << " ";
+    }
+    
+
+    return 0;
+}
