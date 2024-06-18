062ed4fec93b9b83f68191e79d39fcd38d7a433b
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-18 14:16:34 +0600

    learn & implement : Binary search

--- Module-3/Z_Binary_Search.cpp
+++ Module-3/Z_Binary_Search.cpp
@@ -5,40 +5,55 @@ int main()
 {
     int n, t;
     cin >> n >> t;
+
+    // create an array of n size
     int ar[n];
 
+    // take input of array
     for (int i = 0; i < n; i++)
     {
         cin >> ar[i];
     }
 
-    sort(ar, ar+n);
-    
+    // sort array assending order
+    sort(ar, ar + n);
+
+    // while loop for run t times  decrement way
     while (t--)
     {
+        // take input value
         int x;
         cin >> x;
+
+        // create flag for if else
         bool flag = false;
 
-        //leaner search
+        // binary search implementation
+        int l = 0;
+        int r = n - 1;
 
-        // for (int i = 0; i < n; i++)
-        // {
-        //     if (ar[i] == x)
-        //     {
-        //         flag = true;
-        //         break;
-        //     }
-        // }
+        while (l <= r)
+        {
+            int mid_index = (l + r) / 2;
 
-        // binary search 
-        
+                        if (ar[mid_index] == x)
+            {
+                flag = true;
+                break;
+            }
+            else if (x > ar[mid_index])
+            {
+                l = mid_index + 1;
+            }
+            else
+            {
 
+                r = mid_index - 1;
+            }
+        }
 
-
-
-        // print output 
-        if (flag)
+        // print output
+        if (flag == true)
         {
             cout << "found" << endl;
         }
