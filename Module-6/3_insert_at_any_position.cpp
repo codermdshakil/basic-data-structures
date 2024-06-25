178bafc0cabdc46bc5350bcac6c6bd68548c9a81
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-25 12:10:50 +0600

    Copy: Copy insert at any positon function code from gitbook and command it

--- Module-6/3_insert_at_any_position.cpp
+++ Module-6/3_insert_at_any_position.cpp
@@ -58,6 +58,26 @@ void insert_any_position(Node *head, int pos, int v)
          << "Inserted at position " << pos << endl
          << endl;
 }
+//------------------------------------------------
+// from gitbook -- insert at any position function ||
+//------------------------------------------------
+
+// void insert_at_position(Node *&head, int index, int value)
+// {
+
+//     Node *newNode = new Node(value); // নতুন Node বানিয়ে নিলাম 
+
+//     Node *temp = head; // temp পয়েন্টার এ head রেখে দিলাম 
+
+//     for (int i = 1; i <= index - 1; i++)
+//     {
+//         temp = temp->next;
+//     }
+//     // এখানে temp এ যে index এ Node insert করতে হবে তার previous Node আছে
+//     // temp->next এ আছে তার পরের Node 
+//     newNode->next = temp->next; // New_node এর next পয়েন্টার এ  next_node কে রেখে দিলাম
+//     temp->next = newNode; //Previous_Node এর next পয়েন্টার এ  new_node এর এড্রেস রেখে দিলাম
+// }
 
 // print linked list
 void print_link_list(Node *head)
