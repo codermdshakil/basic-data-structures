3588c885db9fe80bb1c456f53be150ec45f04ae6
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-25 21:47:07 +0600

    learn: Take input linked list and print it

---
+++ Module-6/8_input_linked_list.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+//  print linked list
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    cout << "Linked list:";
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, val);
+        }
+    }
+    print_linked_list(head);
+
+    return 0;
+}
