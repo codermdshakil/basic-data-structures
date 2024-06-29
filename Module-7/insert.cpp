0af98de850af0bd269e172853009b2781bb59fd2
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-29 14:33:58 +0600

    Learn: linked list size, insert and error handle using size

---
+++ Module-7/insert.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// create insert function
+void insert(Node *head, int pos, int val)
+{
+
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    return;
+}
+
+int size(Node *head)
+{
+
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+// print linked list function
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    // linked list
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+
+    print_linked_list(head);
+
+    int pos, val;
+    cin >> pos >> val;
+    if (pos > size(head))
+    {
+        cout << endl
+             << "Invalid Index" << endl
+             << endl;
+    }
+    else
+    {
+        insert(head, pos, val);
+    }
+    
+    print_linked_list(head);
+
+    return 0;
+}
