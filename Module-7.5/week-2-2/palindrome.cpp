76b22a414f395e2f2d5c33a56c766a1431c3db4e
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-03 03:40:43 +0600

    Palindrome - single linked list

---
+++ Module-7.5/week-2-2/palindrome.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next = NULL;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// insert at tail
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    // create newNode
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+// print linked list
+void is_palindrome(Node *head)
+{
+    vector<int> v;
+
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        v.push_back(tmp->val);
+        tmp = tmp->next;
+    }
+    vector<int> r;
+    r = v;
+    reverse(r.begin(), r.end());
+
+    if (v == r)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, tail, val);
+    }
+
+    is_palindrome(head);
+
+    return 0;
+}
