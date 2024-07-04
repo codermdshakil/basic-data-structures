f21021d1fabd6fe7e4b4fc8a74c8b82a6568074e
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-04 22:17:55 +0600

    Learn: Create doubly linked list Node

---
+++ Module-9/1_doubly_linked_list.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create double linked list Node using prev, val, next
+class Node
+{
+public:
+    Node *prev;
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->prev = NULL;
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+int main(){
+    
+    return 0;
+}
---
+++ Module-9/test.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create double linked list Node using prev, val, next
+class Node
+{
+public:
+    Node *prev;
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->prev = NULL;
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// print double linked list normal
+void print_normal(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+// print reverse way using tail and prev
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // create dynamic variable using Node
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = b;
+
+    // linked with variables
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+
+    print_normal(head);
+    print_reverse(tail);
+
+    return 0;
+}
