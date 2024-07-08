b2a59534ba046b5ba4e29ee77f3a5c8d3c587f87
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-08 23:04:12 +0600

    Solved Problem - 2 : reverse print doubly linked list

---
+++ Module-10.5/pb_2.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = tail->next;
+    }
+}
+
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, val);
+        }
+    }
+
+    print_reverse(tail);
+
+    return 0;
+}
