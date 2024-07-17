fd6f58566c172ee6fa56788ea0bb1ac8d37f7538
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-18 01:59:09 +0600

    Learn : Queue implement using list

---
+++ Module-14/queue_using_list.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myQueue
+{
+public:
+    list<int> v;
+
+    // add value in back
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    // remove value from front
+    void pop()
+    {
+        v.pop_front();
+    }
+
+    // return top
+    int front()
+    {
+        return v.front();
+    }
+
+    // size of queue
+    int size()
+    {
+        return v.size();
+    }
+
+    // check queue true or false
+    bool empty()
+    {
+        if (v.size() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+
+    myQueue q;
+
+    // take input
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    // print output
+    while (q.empty() != true)
+    {
+        cout << q.front() << endl; // print fast
+        q.pop();                   // remove fast
+    }
+
+    return 0;
+}
