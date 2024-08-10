061131b97115f604e08856832e9ac87667d210e0
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-10 20:15:16 +0600

    Learn : how to access top one time

--- Module-13/practice-stack-queue/stack/3_stack_using_doubly_linked_list.cpp
+++ Module-13/practice-stack-queue/stack/3_stack_using_doubly_linked_list.cpp
@@ -90,24 +90,30 @@ int main()
 
     myStack st;
 
-    // stack size
-    int n;
-    cin >> n;
-
-    // stack elements input
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        st.push(x);
-    }
-
-    // print stack elements
-    while (!st.empty())
+    // When you just want to access top but there is no value in stack
+    if (!st.empty())
     {
         cout << st.top() << endl;
-        st.pop();
     }
+
+    // // stack size
+    // int n;
+    // cin >> n;
+
+    // // stack elements input
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int x;
+    //     cin >> x;
+    //     st.push(x);
+    // }
+
+    // // print stack elements
+    // while (!st.empty())
+    // {
+    //     cout << st.top() << endl;
+    //     st.pop();
+    // }
 
     return 0;
 }
