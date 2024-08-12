57c55708aeb03ea2622f7eea7fec4155a0d5ba55
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-12 21:13:08 +0600

    Learn : take user input in queue and print

--- Module-13/practice-stack-queue/queue/2_queue_using_singly_linked_list.cpp
+++ Module-13/practice-stack-queue/queue/2_queue_using_singly_linked_list.cpp
@@ -80,8 +80,22 @@ int main()
 {
 
     my_queue q;
-    q.push(10);
-    q.pop();
+
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        q.push(x);
+    }
+
+    while (q.emtpy() != true)
+    {
+        cout << q.front() << endl;
+        q.pop();
+
+    }
 
     return 0;
 }
