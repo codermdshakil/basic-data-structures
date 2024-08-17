9fcef92ed6dc7052ed5baa940dbdeda1bde1ea2a
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-17 21:52:56 +0600

    Learn : Create doubly linked list Node

---
+++ Special Recap Sessions for Bangladesh 2.0/Session-2/doubly_linked_list.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create doubly linked list Node
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+
+int main()
+{
+
+    return 0;
+}
