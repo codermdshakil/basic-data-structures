579a3a88377648aff3d954437df133ab63cc937f
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-18 21:50:46 +0600

    Learn : create binary tree node

---
+++ Module-17/1_create_binary_tree.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create binary tree node
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+    // create node using binary  tree node
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+  
+
+
+    return 0;
+}
