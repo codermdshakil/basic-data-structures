c08091738511287fa583f62fe2939e4726c30c2a
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-24 00:06:35 +0600

    Problem Solved : In node is present

---
+++ week-5/Module-19/Is_node_is_present.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// binary tree input
+Node *input_binary_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        // 1. ber kora
+        Node *p = q.front();
+        q.pop();
+
+        // 2. sob kaj ai khane
+
+        // 2.1 take value
+        int l, r;
+        cin >> l >> r;
+
+        // 2.2 create node using input value
+        Node *val_left;
+        Node *val_right;
+
+        // 2.3 error handle
+        if (l == -1)
+        {
+            val_left = NULL;
+        }
+        else
+        {
+            val_left = new Node(l);
+        }
+
+        // 2.4 error handle
+        if (r == -1)
+        {
+            val_right = NULL;
+        }
+        else
+        {
+            val_right = new Node(r);
+        }
+
+        // 2.5 connection left and right
+        p->left = val_left;
+        p->right = val_right;
+
+        // 3. children push kora q te
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+// Level Order Traversal
+void level_order(Node *root, int x)
+{
+
+    // error handle
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    bool flag = false;
+    while (!q.empty())
+    {
+        // 1. ber kore ana [fixed lagbe e]
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        if (f->val == x)
+        {
+            flag = true;
+            break;
+        }
+        else
+        {
+            flag = false;
+        }
+
+        // 3. tar children gulo ke push kore rakha [fixed lagbe e]
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+
+    if (flag)
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+
+        cout << "false" << endl;
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    // connections
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->right = d;
+
+    level_order(root, 40);
+
+    return 0;
+}
