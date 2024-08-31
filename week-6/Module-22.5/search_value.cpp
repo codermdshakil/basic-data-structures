d8e8f7d038a1f92c67d25d9417f13e58d0c71046
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-31 19:25:19 +0600

    Problem Solved: Search node in Binary tree

---
+++ week-6/Module-22.5/search_value.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// binary tree input
+Node *input_binary_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        // 1. ber kora
+        Node *p = q.front();
+        q.pop();
+
+        // 2. sob kaj ai khane
+
+        // 2.1 take value
+        int l, r;
+        cin >> l >> r;
+
+        // 2.2 create node using input value
+        Node *val_left;
+        Node *val_right;
+
+        // 2.3 error handle
+        if (l == -1)
+        {
+            val_left = NULL;
+        }
+        else
+        {
+            val_left = new Node(l);
+        }
+
+        // 2.4 error handle
+        if (r == -1)
+        {
+            val_right = NULL;
+        }
+        else
+        {
+            val_right = new Node(r);
+        }
+
+        // 2.5 connection left and right
+        p->left = val_left;
+        p->right = val_right;
+
+        // 3. children push kora q te
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+// Level Order Traversal
+void search_value(Node *root, int x)
+{
+    // error handle
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ana [fixed lagbe e]
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        if (f->val == x)
+        {
+            cout << f->val << " " <<f->left->val << " " << f->right->val; 
+        } 
+
+        // 3. tar children gulo ke push kore rakha [fixed lagbe e]
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+int main()
+{
+    Node *root = input_binary_tree();
+    search_value(root, 2);
+
+    return 0;
+}
