0c1de0339d1be5d6f81ba1ce3681861548878633
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-05 00:31:40 +0600

    Learn : If value found then ase otherwise nai in using mp.count(key) in Map

--- week-6/Module-23/4_stl_map.cpp
+++ week-6/Module-23/4_stl_map.cpp
@@ -1,26 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
     map<string, int> mp;
     mp.insert({"Sakib Al Hassan", 75});
     mp.insert({"Tamim Iqbal", 19});
     mp.insert({"Rahat", 100});
+    mp.insert({"akib", 0});
     mp.insert({"Shakil", 150});
     mp.insert({"Shamim", 75});
- 
-    // Print all map value 
+
+    // Print all map value
     // for(auto it = mp.begin(); it != mp.end(); it++){
-    //     cout << it->first << " " << it->second << endl;   
+    //     cout << it->first << " " << it->second << endl;
     // }
 
     // Print a specific value
     // cout << mp["Shakil"] << endl;
     // cout << mp["Rahat"] << endl;
     // cout << mp["Shamim"] << endl;
-    
 
+    // if value if found then print ase otherwise print nai
+    // if (mp.count("shakil"))
+    // {
+    //     cout << "Ase : " << mp["Shakil"] << endl;
+    // }
+    // else
+    // {
+    //     cout << "Nai" << endl;
+    // }
 
     return 0;
 }
