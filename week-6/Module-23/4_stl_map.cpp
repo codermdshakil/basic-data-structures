5fbd916420754573d284830a57b8c5e5530a0f6d
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-04 23:20:42 +0600

    Learn : Insert value to map and print value

---
+++ week-6/Module-23/4_stl_map.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    map<string, int> mp;
+    mp.insert({"Sakib Al Hassan", 75});
+    mp.insert({"Tamim Iqbal", 19});
+    mp.insert({"Rahat", 100});
+    mp.insert({"Shakil", 150});
+    mp.insert({"Shamim", 75});
+
+    for(auto it = mp.begin(); it != mp.end(); it++){
+        cout << it->first << " " << it->second << endl;   
+    }
+
+    return 0;
+}
