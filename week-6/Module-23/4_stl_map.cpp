c85df315084441e08810cf0ced7ccb172ab5271b
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-05 00:27:50 +0600

    Learn : using string keyword print value of key in Map

--- week-6/Module-23/4_stl_map.cpp
+++ week-6/Module-23/4_stl_map.cpp
@@ -9,10 +9,18 @@ int main(){
     mp.insert({"Rahat", 100});
     mp.insert({"Shakil", 150});
     mp.insert({"Shamim", 75});
+ 
+    // Print all map value 
+    // for(auto it = mp.begin(); it != mp.end(); it++){
+    //     cout << it->first << " " << it->second << endl;   
+    // }
 
-    for(auto it = mp.begin(); it != mp.end(); it++){
-        cout << it->first << " " << it->second << endl;   
-    }
+    // Print a specific value
+    // cout << mp["Shakil"] << endl;
+    // cout << mp["Rahat"] << endl;
+    // cout << mp["Shamim"] << endl;
+    
+
 
     return 0;
 }
